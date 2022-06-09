e01c694de3777fa6386493913b7d62dbffa92f39
DoctorDalek1963 <69600500+DoctorDalek1963@users.noreply.github.com>
2022-06-09 21:35:48 +0100

    Use command line arguments

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,4 +1,5 @@
 #include <algorithm>
+#include <cstdlib>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -231,14 +232,85 @@ public:
 	}
 };
 
-int main()
+// Class taken from https://stackoverflow.com/a/868894/12985838
+class InputParser
+{
+	public:
+		InputParser (int &argc, char **argv)
+		{
+			for (int i = 1; i < argc; ++i)
+				this->tokens.push_back(std::string(argv[i]));
+		}
+
+		const std::string& getCmdOption(const std::string &option) const
+		{
+			std::vector<std::string>::const_iterator itr;
+			itr = std::find(this->tokens.begin(), this->tokens.end(), option);
+
+			if (itr != this->tokens.end() && ++itr != this->tokens.end())
+				return *itr;
+
+			static const std::string empty_string("");
+			return empty_string;
+		}
+
+		bool cmdOptionExists(const std::string &option) const
+		{
+			return std::find(this->tokens.begin(), this->tokens.end(), option) != this->tokens.end();
+		}
+
+	private:
+		std::vector <std::string> tokens;
+};
+
+// Function taken from https://stackoverflow.com/a/2845275/12985838
+inline bool isInteger(const std::string & s)
+{
+	if (s.empty() || ((!isdigit(s[0])) && (s[0] != '-') && (s[0] != '+')))
+		return false;
+
+	char * p;
+	strtol(s.c_str(), &p, 10);
+
+	return (*p == 0);
+}
+
+int main(int argc, char **argv)
 {
 	srand(time(NULL));
 
 	int fakeArgc = 0;
 	QApplication app(fakeArgc, nullptr);
 
-	SnakeMainWindow window = SnakeMainWindow(16, 12, 50, 5);
+	InputParser input(argc, argv);
+	const std::string &strWidth = input.getCmdOption("-w");
+	const std::string &strHeight = input.getCmdOption("-H");
+	const std::string &strCellsize = input.getCmdOption("-s");
+	const std::string &strFps = input.getCmdOption("-f");
+
+	int width, height, cellsize, fps;
+
+	if (!strWidth.empty() && isInteger(strWidth))
+		width = atoi(strWidth.c_str());
+	else
+		width = 16;
+
+	if (!strHeight.empty() && isInteger(strHeight))
+		height = atoi(strHeight.c_str());
+	else
+		height = 12;
+
+	if (!strCellsize.empty() && isInteger(strCellsize))
+		cellsize = atoi(strCellsize.c_str());
+	else
+		cellsize = 50;
+
+	if (!strFps.empty() && isInteger(strFps))
+		fps = atoi(strFps.c_str());
+	else
+		fps = 5;
+
+	SnakeMainWindow window = SnakeMainWindow(width, height, cellsize, fps);
 	window.show();
 	return app.exec();
 }
