7d0ad6dba5d0c9cfb26041195d0eda4c2d2e0025
DoctorDalek1963 <69600500+DoctorDalek1963@users.noreply.github.com>
2022-06-09 11:55:14 +0100

    Create SnakeMainWindow class with constructor

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,14 +1,76 @@
 #include <QApplication>
 #include <QMainWindow>
+#include <QTimer>
 
-int main(int argc, char *argv[]) {
-	QApplication app(argc, argv);
+enum Direction { UP, DOWN, LEFT, RIGHT };
 
-	QMainWindow window;
+struct Point { int x, y; };
 
-	window.setMinimumSize(250, 150);
-	window.setWindowTitle("Simple example");
+class SnakeMainWindow : public QMainWindow
+{
+	const QColor colourPlayer = QColor(0xe7, 0x03, 0x03);
+	const QColor colourTail = QColor(0xfb, 0x41, 0x7c);
+	const QColor colourApple = QColor(0x09, 0xdd, 0x01);
+
+	const int gridWidth, gridHeight, gridCellSize;
+	int fps;
+
+	Point posPlayer, posApple;
+	std::optional<Direction> dirPlayer;
+
+	std::vector<Point> snakeParts = std::vector<Point>();
+	bool gameOver = false;
+
+	QTimer *timer;
+	bool timerStarted = false;
+
+	void placeApple(void)
+	{
+		this->posApple = this->posPlayer;
+
+		while (posApple.x == posPlayer.x && posApple.y == posPlayer.y) {
+			this->posApple = {
+				rand() % this->gridWidth,
+				rand() % this->gridHeight
+			};
+		}
+	}
+
+private Q_SLOTS:
+	void updateGame(void)
+	{
+		// TODO
+	}
+
+public:
+	SnakeMainWindow(int width, int height, int gridCellSize, int fps)
+		: gridWidth(width), gridHeight(height), gridCellSize(gridCellSize)
+	{
+		this->fps = fps;
+
+		this->posPlayer = {
+			rand() % this->gridWidth,
+			rand() % this->gridHeight
+		};
+		this->dirPlayer = std::nullopt;
+
+		this->posApple = {0, 0};
+		placeApple();
+
+		setFixedSize(this->gridCellSize * this->gridWidth, this->gridCellSize * this->gridHeight);
+		setWindowTitle("Snake");
+
+		this->timer = new QTimer(this);
+		connect(this->timer, &QTimer::timeout, this, &SnakeMainWindow::updateGame);
+	}
+};
+
+int main()
+{
+	int fakeArgc = 0;
+	QApplication app(fakeArgc, nullptr);
+
+	SnakeMainWindow window = SnakeMainWindow(16, 12, 50, 5);
 	window.show();
-
 	return app.exec();
 }
